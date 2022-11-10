37d5c683e39277412fe8b9d6af99221a58d61ff2
Amir Khafizov <superakkgg@gmail.com>
2022-11-10 15:58:52 +0300

    Front
    Starting

---
+++ MarketPlace/MarketPlace/Program.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Data.SqlClient;
+using System.Net;
+using WebServer;
+
+HttpClient client = new();
+HttpListener listener = new();
+client.DefaultRequestHeaders.UserAgent.ParseAdd("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/106.0.0.0 Safari/537.36 Edg/106.0.1370.34");
+listener.Prefixes.Add("http://localhost:1111/");
+listener.Start();
+SqlConnection connection = new(@"Data source= LAPTOP-QHM9MDKR;Initial Catalog=MyDataBase; Integrated Security=True");
+
+while(listener.IsListening)
+{
+    var context = listener.GetContext();
+    var request = context.Request;
+    var response = context.Response;
+    switch (request.Url?.LocalPath)
+    {
+        case "/":
+            await response.ShowFile("WWW/html/mainpage.html");
+            break;
+        default:
+            break;
+    }
+    await context.ShowStatic();
+    response.Close();
+}
+listener.Stop();
---
+++ MarketPlace/MarketPlace/S.cs
@@ -0,0 +1,47 @@
+﻿using System.Net;
+using System.Text;
+
+namespace WebServer
+{
+    public static class S
+    {
+        const string _errorMessage = "error";
+        public static async Task ShowStatic(HttpListenerContext context)
+        {
+            var responce = context.Response;
+            var requestPath = context.Request.RawUrl;
+            var staticPath = Path.Combine(Directory.GetCurrentDirectory(), $"WWW{requestPath}");
+            if (File.Exists(staticPath))
+            {
+                await ShowFile(responce, staticPath);
+            }
+            else
+            {
+                await ShowError(responce);
+            }
+        }
+
+        public static async Task ShowFile(HttpListenerResponse responce, string path)
+        {
+            responce.ContentType = Path.GetExtension(path) switch
+            {
+                ".js" => "application/javascript",
+                ".html" => "text/html",
+                ".css" => "text/css",
+                _ => throw new NotImplementedException()
+            };
+            responce.StatusCode = 200;
+            using var stream = responce.OutputStream;
+            await stream.WriteAsync(File.ReadAllBytes(path));
+        }
+
+        public static async Task ShowError(HttpListenerResponse responce)
+        {
+            responce.ContentType = "text/plain";
+            responce.StatusCode = 404;
+            using var stream = responce.OutputStream;
+            await stream.WriteAsync(Encoding.UTF8.GetBytes(_errorMessage));
+        }
+    }
+}
+
