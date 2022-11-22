22e0641e4ef5a5c62275ccecb32350f2da2c6f18
Amir Khafizov <superakkgg@gmail.com>
2022-11-22 13:32:08 +0300

    Fixing empties after removing products(not ready)

--- MarketPlace/MarketPlace/WWW/js/products.js
+++ MarketPlace/MarketPlace/WWW/js/products.js
@@ -1,26 +1,38 @@
-async function addProductItem(name, information){
-    let productItem = document.getElementById("productItem");
+async function addProductItem(name, information, id){
+    let productItem = document.getElementById("productItemCollector");
     let item = document.createElement("div");
 /*    item.setAttribute('id', "productItem");*/
     item.innerHTML = `
-    <div class="productItem" id="productItem" style="top: ${getCount()}vh;">
+    <div class="productItem" id="${id}product" style="top: ${getCount()}vh; left: 30vw">
         <strong style="display: flex; flex-direction: column">${name}
             <label>
-            <textarea class="productInfo" maxlength="250" readonly>${information}
-            </textarea>
+                <textarea class="productInfo" maxlength="250" readonly>${information}</textarea>
             </label>
         </strong>
     </div>
     `;
+    productsId.push(id);
     productItem.appendChild(item);
 }
-async function redirectMain(){
-    document.location.href="http://localhost:1111/";
+async function removeProduct(id) {
+    //document.getElementById(String(id)+ "product").remove();
+    moveUpElements(id);
+    //document.removeChild(document.getElementById("1"));
+}
+var productsId = [];
+var count = -30;
+function getCount(){
+    count += 55;
+    return count;
 }
 
-var count = 25;
-function getCount(){
-    window.resizeBy("8vw", "80vh");
-    count += 30;
-    return count;
+async function moveUpElements(deletedId){
+    for (i = deletedId; i <= productsId.length; i++){
+        console.log(deletedId + 1);
+        document.getElementById("2product").setAttribute("style", "top:"+ ${} + "vh");
+        document.getElementById("2product").setAttribute("id", "top:"+ ${} + "vh");
+        //document.querySelector('#reg').style.top = '10vh';
+        //document.getElementById("2").style.top = '10vh';
+    }
+    count -= 55;
 }
--- MarketPlace/MarketPlace/WWW/js/productsNotRegistered.js
+++ MarketPlace/MarketPlace/WWW/js/productsNotRegistered.js
@@ -1,6 +1,20 @@
-async function addProductItem(){
-    alert(1);
+async function addProductItem(name, information){
+    let productItem = document.getElementById("productItemCollector");
+    let item = document.createElement("div");
+    /*    item.setAttribute('id', "productItem");*/
+    item.innerHTML = `
+    <div class="productItem" id="productItem" style="top: ${getCount()}vh; left: 30vw">
+        <strong style="display: flex; flex-direction: column">${name}
+            <label>
+                <textarea class="productInfo" maxlength="250" readonly>${information}</textarea>
+            </label>
+        </strong>
+    </div>
+    `;
+    productItem.appendChild(item);
 }
-async function redirectMain(){
-    document.location.href="http://localhost:1111/";
+var count = -30;
+function getCount(){
+    count += 55;
+    return count;
 }
